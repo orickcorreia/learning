1dad8e391b4def98e752657822293c9c38348e07
Ricardo Correia <orickcorreia@gmail.com>
2020-08-11 11:13:50 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 8000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Carai');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Iniciando o servidor http://${hostname}:${port}/`);
 });
